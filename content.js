a0d793b80a8e9319260c07763cfeecdc096b2166
Qixuan-Yu <maciophobia@gmail.com>
2019-09-21 11:22:58 -0500

    Update content.js
    Extension can now extract text from page

--- content.js
+++ content.js
@@ -5,7 +5,26 @@
 //     $("p").html(' s')
 //     );
 console.log("Chrome extension is working!");
-alert('This is an alert');
+
+console.log(document.body.innerText);
+
+var mutationObserver = new MutationObserver(function(mutations) {
+    if (mutations !== null) {
+        console.log(document.body.innerText);
+    }
+});
+
+// Starts listening for changes in the root HTML element of the page.
+mutationObserver.observe(document.documentElement, {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+});
+
+
 // let elts = document.getElementsByTagName('p');
 // for(var i = 0; i< elts.length; i++){
 //     elts[i].style['background-color'] = '#F0C';

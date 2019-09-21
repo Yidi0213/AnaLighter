543eae88edf8f80c64f5734bd8b08093de0d4f3e
Yidi0213 <yw73@rice.edu>
2019-09-21 10:29:23 -0500

    Submitted manifest

---
+++ content.js
@@ -0,0 +1,14 @@
+// console.log("It is rainning today");
+
+// $(".alertClick").click(()=>alert('Lol'));
+// $(".alertClick").on('mouseover',
+//     $("p").html(' s')
+//     );
+console.log("Chrome extension is working!");
+alert('This is an alert');
+// let elts = document.getElementsByTagName('p');
+// for(var i = 0; i< elts.length; i++){
+//     elts[i].style['background-color'] = '#F0C';
+// }
+// $(window).load(()=> $("p").css('background-color', '#F0C'));
+// $("p").css('background-color','#F0C');

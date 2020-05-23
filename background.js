bc657bef5e3819fd5d9a2ab02c36154971bab857
Yidi Wang <yw73@rice.edu>
2020-05-23 17:25:21 -0500

    added back id button

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.runtime.onMessage.addListener((msg,sender,sendReponse) =>{
             fetch(url)
             .then(res=>res.json())
             .then(data=>{
-                console.log(data);
+                // console.log(data);
                 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                                 chrome.tabs.sendMessage(tabs[0].id, {
                                     type: "keywords",
